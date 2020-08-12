16ace6d72790b9c30f6f2f837833e70972e71068
akritig187 <44704264+akritig187@users.noreply.github.com>
2020-08-12 22:34:36 +0530

    Update Jobs.js

--- src/Jobs.js
+++ src/Jobs.js
@@ -38,7 +38,7 @@ export default function Jobs({jobs}) {
         const c = document.documentElement.scrollTop || document.body.scrollTop;
         if (c > 0) {
           window.requestAnimationFrame(scrollToTop);
-          window.scrollTo(0, c - c / 8);
+          window.scrollTo(0, 0);
         }
       };
 
